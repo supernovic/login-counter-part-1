88696011139f1c27c10df9a026ad05eeff872872
System Administrator <root@unknown20c9d0859ed5.gateway.2wire.net>
2013-02-11 00:13:42 -0800

    added status code to json response

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,9 +8,9 @@ class UserController < ApplicationController
     respond_to do |format|
       error_code = @user.login(@user.username, @user.password)
       if error_code > 0
-        format.json { render :json => {:errCode => error_code, :count => @user.count} }
+        format.json { render :json => {:errCode => error_code, :count => @user.count}, :status => 200  }
       else
-        format.json { render :json => {:errCode => error_code} }
+        format.json { render :json => {:errCode => error_code}, :status => 200  }
       end
     end
   end
@@ -20,9 +20,9 @@ class UserController < ApplicationController
     respond_to do |format|
       error_code = @user.add(@user.username, @user.password)
       if error_code > 0
-        format.json { render :json => {:errCode => error_code, :count => @user.count} }
+        format.json { render :json => {:errCode => error_code, :count => @user.count}, :status => 200 }
       else
-        format.json { render :json => {:errCode => error_code} }
+        format.json { render :json => {:errCode => error_code}, :status => 200  }
       end
     end
   end
@@ -31,14 +31,14 @@ class UserController < ApplicationController
     @user = User.new(params[:user])
     if @user.TESTAPI_resetFixture() > 0
       respond_to do |format|
-        format.json{ render :json => {:errCode} }
+        format.json{ render :json => {:errCode}, :status => 200  }
       end
     end
   end
   
   def test
     respond_to do |format|
-      format.json { render :json => {:totalTests => 10, :nrFailed => 0, :output => "success"} }
+      format.json { render :json => {:totalTests => 10, :nrFailed => 0, :output => "success"}, :status => 200  }
     end
   end
 end
